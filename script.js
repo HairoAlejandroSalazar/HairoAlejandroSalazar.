df099536e19c39112fca7fbe72b3502844a34010
HairoAlejandroSalazar <162625319+HairoAlejandroSalazar@users.noreply.github.com>
2024-03-08 14:42:52 -0600

    Add files via upload
    Seguridad en Computo

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("cambiarColorBtn").addEventListener("click", function() {
+    document.body.style.backgroundColor = "lightblue";
+});
+
+document.getElementById("cambiarTamanoBtn").addEventListener("click", function() {
+    var imagen1 = document.getElementById("imagen1");
+    imagen1.style.width = "400px";
+});
