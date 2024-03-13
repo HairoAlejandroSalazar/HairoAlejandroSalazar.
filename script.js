bde38d4c69ca95565cbe662552fc4b4b7e787843
HairoAlejandroSalazar <162625319+HairoAlejandroSalazar@users.noreply.github.com>
2024-03-12 23:28:18 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("cambiarColorBtn").addEventListener("click", function() {
-    document.body.style.backgroundColor = "lightblue";
-});
-
-document.getElementById("cambiarTamanoBtn").addEventListener("click", function() {
-    var imagen1 = document.getElementById("imagen1");
-    imagen1.style.width = "400px";
-});
